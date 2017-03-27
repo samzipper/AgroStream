6b91f1ff925060e1281cc4333a0b068bc3b5201e
Sam Zipper <samuelczipper@gmail.com>
2017-03-27 08:18:57 -0400

    Add script to collect tweets from specific date range
    This can be used when regularly scheduled scripts do not run for some
    reason (such as the computer being turned off).

--- LoadTweets.R
+++ LoadTweets.R
@@ -15,6 +15,7 @@ require(stringr)
 require(maptools)
 require(DBI)
 require(ROAuth)
+require(dplyr)
 
 # output directory: this is where the SQLite database is
 #out.dir <- "C:/Users/Sam/Dropbox/Work/AgroStream/"
@@ -32,5 +33,17 @@ df <- dbReadTable(db, "tweets")
 # when you're done, disconnect from database (this is when the data will be written)
 dbDisconnect(db)
 
+# plot of tweets by day
+df$created <- ymd_hms(df$created)
+df$DOY <- yday(df$created)
+df.d <- summarize(group_by(df, DOY),
+                  tweets = sum(is.finite(lat.location)))
+
 # print most recent tweet
 print(paste0("Last tweet: ", df$created[dim(df)[1]]))
+
+p.bar.tweets.DOY <-
+  ggplot(df.d, aes(x=DOY, y=tweets)) +
+  geom_bar(stat="identity") +
+  theme_bw() +
+  theme(panel.grid=element_blank())
---
+++ SearchAndStoreTweets_SpecificDates.R
@@ -0,0 +1,254 @@
+## SearchAndStoreTweets_SpecificDates.R
+#' This script is intended to:
+#'  (1) search Twitter for a keyword or set of keywords
+#'  (2) download all matching Tweets
+#'  (3) extract the location of the tweeter via Google Maps
+#'  (4) save the output as a CSV file
+
+rm(list=ls())
+
+# path to git directory
+git.dir <- "C:/Users/Sam/WorkGits/AgroStream/"
+
+# load packages
+require(twitteR)
+require(lubridate)
+require(ggmap)
+require(stringr)
+require(maptools)
+require(DBI)
+require(ROAuth)
+
+# search string: what will you search twitter for?
+search.str <- "((corn OR soy OR wheat) AND (plant OR planting OR planted OR plants OR #plant17 OR #plant2017)) OR #corn17 OR #corn2017 OR #soy17 OR #soy2017 OR #wheat17 OR #wheat2017"
+
+# output directory: save to Dropbox, not git repository, so it's automatically backed up
+# this is also where authentication info is stored
+#out.dir <- "C:/Users/Sam/Dropbox/Work/AgroStream/"
+out.dir <- "D:/Dropbox/Work/AgroStream/"
+
+# path to save output data
+path.out <- paste0(out.dir, "TweetsOut.sqlite")
+
+# path to save the screen output
+path.sink <- paste0(out.dir, "TweetsOut_Screen_", format(Sys.time(), "%Y%m%d-%H%M"), ".txt")
+
+## launch sink file, which will store screen output 
+# this is useful when automating, so it can be double-checked later
+# to make sure nothing weird happened
+s <- file(path.sink, open="wt")
+sink(s, type="message")
+
+# path to a CSV file with a list of all countries 
+# (downloaded from: http://blog.plsoucy.com/2012/04/iso-3166-country-code-list-csv-sql/ )
+path.countries <- paste0(git.dir, "AllCountries.csv")
+
+# relative path to authentication info (this is in .gitignore
+# so not shared publicly). these are obtained from twitter/google
+# when you create your app.
+path.auth.t <- paste0(out.dir, "TwitterAuth.txt")
+
+# read in authentication info - file has three lines of 
+# comments followed by:
+#   [1] consumer key (API key)
+#   [2] consumer secret (API secret)
+#   [3] access token
+#   [4] access secret
+auth.t <- read.table(path.auth.t, skip=3, stringsAsFactors=F)[,1]    # read in as vector
+
+# set up authentication
+options(httr_oauth_cache=T)   # this will store authentication as a local file
+setup_twitter_oauth(auth.t[1], auth.t[2], auth.t[3], auth.t[4])
+
+# get today/yesterday dates
+date_start <- as.Date(ymd("2017-03-23"))  # this date is included
+date_end <- as.Date(ymd("2017-03-26"))    # this date is not included
+
+# search twitter!
+tweets <- searchTwitter(search.str, 
+                        n=10000, 
+                        geocode='39.833333,-98.583333,1500mi',
+                        resultType="recent",
+                        since=as.character(date_start),
+                        until=as.character(date_end),
+                        retryOnRateLimit=5000)
+
+# get rid of retweets
+tweets <- strip_retweets(tweets, strip_manual=T, strip_mt=T)
+
+# put into data frame (only categories we care about)
+df <- twListToDF(tweets)[,c("text", "created", "id", "screenName", "isRetweet", "longitude", "latitude")]
+
+# convert text to UTF-8 to deal with weird characters
+df$text <- sapply(df$text, function(row) iconv(row, to='UTF-8'))
+df$text <- gsub("\n", " ", df$text)
+
+# get rid of tweets referring to The Tribez (an android game where people plant things)
+df <- subset(df, !grepl(tolower("The Tribez"), tolower(df$text)))
+
+## using Google Maps API, get estimated geographic coordinates based on user location
+# limit of 2500/day! so, get clean location as much as possible first to minimize calls to API
+
+# get user location
+userInfo <- lookupUsers(df$screenName)
+df.users <- twListToDF(userInfo)
+
+# trim to only users with location info
+df.users <- df.users[df.users$location != "",]
+
+# replace % in user location with blank so geocode doesn't get messed up
+df.users$location <- gsub("%", " ",df.users$location)
+
+# trim leading/trailing white space
+df.users$location <- trimws(df.users$location)
+
+## filter locations by US only (partial string matching)
+# load countries
+df.countries <- read.csv(path.countries, stringsAsFactors=F)
+df.countries <- subset(df.countries, code != "US")   # get rid of US from list
+
+# add some more countries/other things to filter
+countries <- c(df.countries$name, 
+               "Netherlands", "México", "Guam", 
+               "Alberta", "Saskatchewan", "British Columbia", "Yukon Territories", "Ontario", "Quebec",
+               "Nunavut", "Northwest Territories", "Yukon Territory", "Prince Edward Island", "Newfoundland",
+               "Alaska", "Hawaii", "Africa", "Asia", "Europe", "Australia")
+
+# eliminate for any location that includes a country name that's not the US
+df.users <- df.users[
+  unlist(lapply(X=df.users$location, 
+                FUN=function(x) sum(str_detect(tolower(x), tolower(countries))))
+  )==0, ]
+
+## filter locations to eliminate any that are just a large geographic region name (exact matching)
+big.geo <- c("United Nations", "Earth", "United States", "USA", "US", "America", 
+             "North America", "South America")
+
+# get rid of locations that are just a state name
+df.users <- df.users[!(df.users$location %in% big.geo), ]
+
+# get unique locations
+locations <- unique(df.users$location)
+
+# call geocode
+geo.out <- geocode(locations, source="google", output="all")
+
+## filter output
+# status check: did geocode find a location?
+check.status <- sapply(geo.out, function(x) x["status"]=="OK" & length(x["status"])>0)
+
+# status check: is location ambiguous?
+check.ambig <- sapply(lapply(geo.out, lapply, length), function(x) x["results"]=="1")
+
+# status check: is location resolved to state level?
+# acceptable google address component codes, from https://developers.google.com/maps/documentation/geocoding/intro
+add.comp.state <- c("locality", "postal_code", "neighborhood", "park", "sublocality", "locality",
+                    paste0("administrative_area_level_", seq(1,5)))
+
+state.resolved <- function(i.location, geocodes=geo.out){
+  # custom function to determine if any subcounty address component exists
+  #   input, i.location, is the index of a point in the geo.out list
+  #   output will be a logical (T/F)
+  
+  # check if it found any results
+  if (length(geocodes[[i.location]]$results)>0){
+    
+    # figure out number of address components returned for this location
+    n.add.comp <- length(geocodes[[i.location]]["results"]$results[[1]]$address_components)
+    
+    # extract address component types for this location
+    add.comp.types <- unlist(sapply(1:n.add.comp, function(x) unlist(geocodes[[i.location]]["results"]$results[[1]]$address_components[[x]]$types)))
+    
+    # see if any address component types are at subcounty level
+    return(sum(add.comp.types %in% add.comp.state)>0)
+    
+  } else {
+    # if no results found, output is false
+    
+    return(FALSE)
+    
+  }
+}
+
+check.state <- unlist(lapply(1:length(locations), FUN=state.resolved))   # apply state check function
+
+# combine all checks into a single logical
+check.all <- check.status & check.ambig & check.state
+
+# trim geo.out
+geo.out <- geo.out[check.all]
+
+## make final locations data frame
+df.locations <- data.frame(
+  location = locations[check.all],
+  lat.location = sapply(geo.out, function(x) x["results"]$results[[1]]$geometry$location$lat),
+  lon.location = sapply(geo.out, function(x) x["results"]$results[[1]]$geometry$location$lng)
+)
+
+# add location info back to user data frame
+df.users <- merge(df.users[c("location", "description", "screenName")], df.locations, by="location", all.x=T)
+
+# make output data frame including tweet, user, location, etc.
+df.out <- merge(df, df.users, by="screenName", all.x=T)
+
+# trim output data frame to only those with locations (either geotagged or from google)
+df.out <- df.out[(is.finite(df.out$latitude) & is.finite(df.out$longitude)) | 
+                   (is.finite(df.out$lat.location) & is.finite(df.out$lon.location)), ]
+
+# get rid of commas
+df.out$text <- gsub(",", " ", df.out$text)
+df.out$location <- gsub(",", " ", df.out$location)
+df.out$description <- gsub(",", " ", df.out$description)
+
+# get rid of line breaks
+df.out$text <- gsub("\n", " ", df.out$text)
+df.out$location <- gsub("\n", " ", df.out$location)
+df.out$description <- gsub("\n", " ", df.out$description)
+
+# get rid of URLs
+removeURL <- function(x) gsub("http[[:alnum:][:punct:]]*", "", x)
+df.out$text <- unlist(lapply(df.out$text, removeURL))
+
+# trim leading/trailing white space
+df.out$text <- trimws(df.out$text)
+
+# get rid of duplicate tweets
+df.out <- df.out[!duplicated(df.out[c("screenName", "text")]), ]
+
+# put in order
+df.out <- df.out[order(df.out$id), ]
+
+# convert dates to character string for database
+df.out$created <- as.character(df.out$created)
+
+## put into database
+# create/connect to database
+db <- dbConnect(RSQLite::SQLite(), path.out)
+
+# add data frame to database (if it doesn't exist, it will be created)
+dbWriteTable(db, "tweets", df.out, append=T)
+
+# if you want to read in a data frame from your db to check...
+#df.test <- dbReadTable(db, "tweets")
+
+# when you're done, disconnect from database (this is when the data will be written)
+dbDisconnect(db)
+
+# print status update
+print(paste0(dim(df.out)[1], " tweets added to database"))
+
+# close sink
+close(s)
+sink()
+sink(type="message")
+close(s)
+
+# # make a plot
+# state_map <- map_data("state")
+# p.map <-
+#   ggplot(data=df.out, aes(x=lon.location, y=lat.location)) +
+#   geom_path(data=state_map, color="blue", aes(x=long, y=lat, group=factor(region))) +
+#   geom_point(shape=21) +
+#   coord_map() +
+#   theme_bw() +
+#   theme(panel.grid=element_blank())
